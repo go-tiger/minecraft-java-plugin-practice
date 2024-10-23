873248cb426fb64d1d7849409a69b4f735c180cb
go-tiger <dev.gotiger@gmail.com>
2024-10-23 09:25:56 +0900

    feat: config .yml 파일 생성

--- src/main/java/dev/gotiger/pluginPractice/PluginPractice.java
+++ src/main/java/dev/gotiger/pluginPractice/PluginPractice.java
@@ -1,12 +1,24 @@
 package dev.gotiger.pluginPractice;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class PluginPractice extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        String path = "test.yml";
+        File newFile = new File(getDataFolder(), path);
+        if (!newFile.exists() && getResource(path) != null) {
+            saveResource(path, false);
+        }
+        FileConfiguration config = YamlConfiguration.loadConfiguration(newFile);
+
         getLogger().info("플러그인 활성화!");
     }
 
